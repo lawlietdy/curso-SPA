4438a2721afd4304f89727978de825c48cf6eba5
Angel Rodriguez <rodriguezaguirreangelt@gmail.com>
2020-11-25 02:12:20 -0600

    Hasta aqui ya esta todo terminado

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -27,7 +28,13 @@ module.exports = {
                 template: './public/index.html',
                 filename: './index.html',
             }
-        )
+        ),
+        new CopyWebpackPlugin({
+            patterns:[{
+                from: './src/styles/styles.css',
+                to: ''
+            }],
+        })
     ]
     
 }
